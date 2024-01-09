1d328fabf54f514a84b309a1641ff5d9121fa7d9
mlaferreiraweb <mlaferreira.marcello@gmail.com>
2024-01-09 15:59:38 -0300

    exercicio 01 feito da aula de JS

--- resposta-01.js
+++ resposta-01.js
@@ -10,7 +10,10 @@ Nesse desafio, você irá solicitar ao usuário que ele insira **dois números**
 - [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
 - [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~.
  * 
- */let numberOne = prompt('DIGITE O PRIMEIRO NÚMERO:');
+
+ */
+
+let numberOne = prompt('DIGITE O PRIMEIRO NÚMERO:');
 let numberTwo = prompt('DIGITE O SEGUNDO NÚMERO');
 
 let sum     = Number(numberOne) + Number(numberTwo);
@@ -19,19 +22,18 @@ let mult    = Number(numberOne) * Number(numberTwo);
 let div     = (Number(numberOne) / Number(numberTwo)).toFixed(2);
 let restDiv = Number(numberOne) % Number(numberTwo);
 
+
+alert(`SOMA DOS DOIS ( ${numberOne} e ${numberTwo}) NÚMERO DIGITADOS É: ${sum}`)
+alert(`SUBTRAÇÃO DOS DOIS ( ${numberOne} e ${numberTwo}) NÚMERO DIGITADOS É: ${sub}`)
+alert(`MULTIPLICAÇÃO DOS DOIS ( ${numberOne} e ${numberTwo}) NÚMERO DIGITADOS É: ${mult}`)
+alert(`DIVISÃO DOS DOIS ( ${numberOne} e ${numberTwo}) NÚMERO DIGITADOS É: ${div}`)
+alert(`RESTO DA DIVISÃO DOS DOIS ( ${numberOne} e ${numberTwo}) NÚMERO DIGITADOS É: ${restDiv}`)
+
 if (numberOne === numberTwo) {
-    alert(`Os números são iguais!`);
+    alert(`OS NÚMEROS SÃO IGUAIS!`);
 } else {
-    let parity = sum % 2 === 0 ? 'par' : 'ímpar';
-
+    let parity = sum % 2 === 0 ? 'PAR' : 'ÍMPAR';
     alert(`Resultados:
-    - Números Digitados : ${numberOne} e ${numberTwo}
-    - Soma: ${sum}
-    - Subtração: ${sub}
-    - Multiplicação: ${mult}
-    - Divisão: ${div}
-    - Resto da divisão: ${restDiv}
-    - A soma dos dois números é ${parity}`);
+    - OS DOIS NÚMEROS DIGITADOS : ${numberOne} E ${numberTwo}     
+    - A SOMA DOS NÚMEROS É: ${parity}`);
 }
-
-
